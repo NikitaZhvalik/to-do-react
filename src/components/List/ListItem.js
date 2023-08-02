b6c005319a7fa02eec11d795323797a0a155eee9
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-08-02 15:28:23 +1000

    02: раздел-е list на комп-ты + функц сдел и важн

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,12 +1,48 @@
-function ListItem(props) {
-    return (
-        <li className="todo-item">
-            <span className="todo-item-text" >{props.el.title}</span>
-            <div className="btn-group">
-                <button role="button" className="btn btn-outline-dark btn-sm">Важное</button>
-                <button role="button" className="btn btn-outline-danger btn-sm">Удалить</button>
-            </div>
-        </li>);
+import React from 'react';
+
+class ListItem extends React.Component {
+    state = {
+        important: false,
+        done: false,
+    }
+
+    onImportantClick = () => {
+		this.setState((state) => {
+			return {
+				important: !state.important,
+			}
+		})
+    }
+
+    onDoneClick = () => {
+        this.setState((state) => {
+            return {
+                done: !state.done,
+            }
+        })
+    }
+
+
+	render() {
+        let classNames = "todo-item";
+
+        if (this.state.important) {
+            classNames += ' important';
+        }
+
+        if (this.state.done) {
+            classNames += ' done';
+        }
+
+		return (
+            <li className={classNames}>
+                <span onClick={this.onDoneClick} className="todo-item-text" >{this.props.el.title}</span>
+                <div className="btn-group">
+                    <button onClick={this.onImportantClick} role="button" className="btn btn-outline-dark btn-sm">Важное</button>
+                    <button role="button" className="btn btn-outline-danger btn-sm">Удалить</button>
+                </div>
+            </li>);
+	}
 }
 
 export default ListItem;
